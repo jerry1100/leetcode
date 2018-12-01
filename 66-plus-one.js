3659e20edafc966a4d066255cf9812648989863d
jerry1100 <jwu1100@gmail.com>
2018-12-01 11:02:27 -0800

    finish 66

---
+++ 66-plus-one.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+function plusOne(digits) {
+  let carry = 1;
+
+  // Process digits from right to left
+  for (let i = digits.length - 1; i >= 0; i--) {
+    const sum = digits[i] + carry;
+    carry = sum > 9 ? 1 : 0;
+    digits[i] = sum % 10;
+  }
+
+  if (carry) {
+    digits.unshift(1);
+  }
+
+  return digits;
+};
