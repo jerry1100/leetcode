089a89a76d31fbc0dd1c87a38ec52f53f2f413c4
jerry1100 <jwu1100@gmail.com>
2018-12-01 12:42:18 -0800

    add 14

---
+++ 14-longest-common-prefix.js
@@ -0,0 +1,36 @@
+/**
+ * Time: O(n*m)
+ * Space: O(1)
+ * 
+ * n - # of strings
+ * m - # of chars in shortest string
+ */
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+function longestCommonPrefix(strs) {
+  if (!strs.length) {
+    return '';
+  }
+
+  // Method to recursively get the lcp starting at index
+  const lcpAtIndex = index => {
+    const charToTest = strs[0].charAt(index);
+    if (!charToTest) {
+      return '';
+    }
+
+    // Check if all strings match the test char
+    for (let i = 1; i < strs.length; i++) {
+      if (strs[i].charAt(index) !== charToTest) {
+        return '';
+      }
+    }
+
+    return charToTest + lcpAtIndex(index + 1);
+  };
+
+  return lcpAtIndex(0);
+};
