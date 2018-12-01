da7e0bae45e03d2c911a56ec7b5a31600e9da749
jerry1100 <jwu1100@gmail.com>
2018-12-01 15:22:49 -0800

    add 3

---
+++ 3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,30 @@
+/**
+ * Time: O(n)
+ * Space: O(n)
+ * s - length of string
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function lengthOfLongestSubstring(s) {
+  const map = new Map();
+  let left = 0;
+  let maxLength = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s.charAt(i);
+    const right = i + 1;
+
+    if (map.has(char) && map.get(char) >= left) {
+      left = map.get(char) + 1; // move left boundary past previous occurrence
+    } else if (right - left > maxLength) {
+      maxLength = right - left;
+    }
+
+    map.set(char, i);
+  }
+
+  return maxLength;
+};
