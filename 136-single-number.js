f93b6c61269540029015eded8875aa8415e6bf85
jerry1100 <jwu1100@gmail.com>
2018-12-01 15:34:56 -0800

    add 136

---
+++ 136-single-number.js
@@ -0,0 +1,13 @@
+/**
+ * Time: O(n)
+ * Space: O(1)
+ * n - # of elements in nums
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function singleNumber(nums) {
+  return nums.reduce((total, current) => total^current);
+};
