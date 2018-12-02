369cfd59a84f02e1623af3ba945c639db8c339c2
jerry1100 <jwu1100@gmail.com>
2018-12-02 00:10:26 -0800

    add 338

---
+++ 338-counting-bits.js
@@ -0,0 +1,23 @@
+/**
+ * Time: O(n)
+ * Space: O(n)
+ * n - value of num
+ */
+
+/**
+ * @param {number} num
+ * @return {number[]}
+ */
+function countBits(num) {
+  const bits = [0];
+  let nextPowerOf2 = 1;
+
+  // Pattern: sol(i) = sol(i - lastPowerOf2) + 1
+  for (let i = 1; i <= num; i++) {
+    if (i === nextPowerOf2) {
+      nextPowerOf2 *= 2;
+    }
+    bits[i] = bits[i - nextPowerOf2 / 2] + 1;
+  }
+  return bits;
+};
