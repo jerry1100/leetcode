04836a1a75690a2d389ce713a57870c7caed960f
jerry1100 <jwu1100@gmail.com>
2018-12-01 23:08:38 -0800

    add 258

---
+++ 258-add-digits.js
@@ -0,0 +1,15 @@
+/**
+ * Time: O(1)
+ * Space: O(1)
+ */
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+function addDigits(num) {
+  if (!num) {
+    return 0;
+  }
+  return num % 9 || 9; // pattern goes 1-9 then repeats
+};
