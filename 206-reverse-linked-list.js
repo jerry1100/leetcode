b2526229da9a353249b3699adc6b704947455fa4
jerry1100 <jwu1100@gmail.com>
2018-12-01 17:21:12 -0800

    add 206

---
+++ 206-reverse-linked-list.js
@@ -0,0 +1,30 @@
+/**
+ * Time: O(n)
+ * Space: O(1)
+ * n - # of nodes
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function reverseList(head) {
+  let prev = null;
+  
+  // Flip each node's next around
+  while (head) {
+    const next = head.next;
+    head.next = prev;
+    prev = head;
+    head = next;
+  }
+
+  return prev;
+};
