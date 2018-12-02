1c410df725751b8af9e9488c08fd4893c52432ba
jerry1100 <jwu1100@gmail.com>
2018-12-02 11:28:09 -0800

    add 342

---
+++ 342-power-of-four.js
@@ -0,0 +1,26 @@
+/**
+ * Time: O(1)
+ * Space: O(1)
+ */
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+function isPowerOfFour(num) {
+  if (num < 1) {
+    return false;
+  }
+
+  // If not a power of 2
+  if (num & (num - 1)) {
+    return false;
+  }
+
+  // If the 1 bit is at an even position
+  if (num & 0b0101010101010101010101010101010) {
+    return false;
+  }
+
+  return true;
+};
