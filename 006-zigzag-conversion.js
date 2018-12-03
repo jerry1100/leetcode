301e1136c54da9b378250a90e1a2d6dfe3de0dc1
jerry1100 <jwu1100@gmail.com>
2018-12-02 21:34:14 -0800

    add 6

---
+++ 006-zigzag-conversion.js
@@ -0,0 +1,33 @@
+/**
+ * Time: O(n)
+ * Space: O(n)
+ * n - length of string
+ */
+
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+function convert(s, numRows) {
+  if (numRows === 1) {
+    return s;
+  }
+
+  const strs = Array(numRows).fill('');
+  let step = 1;
+  let row = 0;
+
+  // Add each letter to its corresponding row
+  for (let i = 0; i < s.length; i++) {
+    strs[row] += s.charAt(i);
+    row += step;
+
+    // Reached the edge so turn around
+    if (row === 0 || row === numRows - 1) {
+      step *= -1;
+    }
+  }
+
+  return strs.reduce((total, current) => total + current); // join all the rows
+};
