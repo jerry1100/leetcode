710ac5a6877b2c493f77747222b4da677271aa45
jerry1100 <jwu1100@gmail.com>
2018-12-02 19:31:59 -0800

    add 371

---
+++ 371-sum-of-two-integers.js
@@ -0,0 +1,20 @@
+/**
+ * Time: O(sizeof(number))
+ * Space: O(1)
+ */
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+function getSum(a, b) {
+  const sum = a ^ b; // XOR derives the sum bits, without carry
+  const carry = (a & b) << 1; // AND derives the carry bits
+
+  if (!carry) {
+    return sum;
+  }
+
+  return getSum(sum, carry);
+};
