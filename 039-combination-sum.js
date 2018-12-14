1f408daf72c8128def2ca6d4cfd0239e20f97a60
jerry1100 <jwu1100@gmail.com>
2018-12-14 00:09:37 -0800

    add 39

---
+++ 039-combination-sum.js
@@ -0,0 +1,35 @@
+/**
+ * Time: O(m^2 * 2^n)
+ * Space: O(m * 2^n)
+ * n - # of candidates
+ * m - target
+ */
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+function combinationSum(candidates, target) {
+  const validSets = [];
+
+  const findSets = (testSet, remainder, start) => {
+    if (!remainder) {
+      return validSets.push(testSet.slice()); // copy array by value
+    }
+
+    if (remainder < 0) {
+      return;
+    }
+
+    // Track start to prevent going backwards, avoiding duplicates
+    for (let i = start; i < candidates.length; i++) {
+      testSet.push(candidates[i]);
+      findSets(testSet, remainder - candidates[i], i);
+      testSet.pop();
+    }
+  };
+
+  findSets([], target, 0);
+  return validSets;
+};
