2cbf7a84a7e8dfb819f03ee552d4369c4e63021d
jerry1100 <jwu1100@gmail.com>
2018-12-15 11:58:32 -0800

    add 78

---
+++ 078-subsets.js
@@ -0,0 +1,26 @@
+/**
+ * Time: O(2^n)
+ * Space: O(2^n)
+ * n - # of nums
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+function subsets(nums) {
+  const powerSet = [];
+
+  const getSubset = (set, start) => {
+    powerSet.push(set.slice()); // copy by value
+
+    for (let i = start; i < nums.length; i++) {
+      set.push(nums[i]);
+      getSubset(set, i + 1);
+      set.pop();
+    }
+  };
+
+  getSubset([], 0);
+  return powerSet;
+};
