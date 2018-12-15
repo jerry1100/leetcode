d56463c9f35ce0e73a7e9361f29abf98cc8c1a32
jerry1100 <jwu1100@gmail.com>
2018-12-15 10:51:45 -0800

    add 46

---
+++ 046-permutations.js
@@ -0,0 +1,32 @@
+/**
+ * Time: O(n * n!)
+ * Space: O(n * n!)
+ * n - # of nums
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+function permute(nums) {
+  const permutations = [];
+
+  const getPermutations = set => {
+    if (set.length === nums.length) {
+      return permutations.push(set.slice()); // copy array by value
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+      if (set.includes(nums[i])) {
+        continue;
+      }
+
+      set.push(nums[i]);
+      getPermutations(set);
+      set.pop(nums[i]);
+    }
+  };
+
+  getPermutations([]);
+  return permutations;
+};
