9e88b38cba4137db8d2d5d2a7a066b1f8b7534ea
jerry1100 <jwu1100@gmail.com>
2018-12-15 23:45:32 -0800

    add 51

---
+++ 051-n-queens.js
@@ -0,0 +1,43 @@
+/**
+ * Time: O(n!)
+ * Space: O(n) per solution
+ * n - # of queens
+ */
+
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+function solveNQueens(n) {
+  const solutions = [];
+
+  const isValid = (colPlacements, col) => {
+    for (let row = 0; row < colPlacements.length; row++) {
+      const diffX = Math.abs(colPlacements[row] - col);
+      const diffY = colPlacements.length - row;
+      if (!diffX || diffX === diffY) { // same column or diagonal
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const explore = (row, colPlacements) => {
+    if (row === n) {
+      return solutions.push(colPlacements.slice()); // copy by value
+    }
+    for (let col = 0; col < n; col++) {
+      if (!isValid(colPlacements, col)) {
+        continue;
+      }
+      colPlacements.push(col);
+      explore(row + 1, colPlacements);
+      colPlacements.pop();
+    }
+  };
+
+  explore(0, []);
+  return solutions.map(solution =>
+    solution.map(col => '.'.repeat(col) + 'Q' + '.'.repeat(n - col - 1))
+  );
+};
