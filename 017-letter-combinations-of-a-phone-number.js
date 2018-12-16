1af8fa3a35c281d80a80baf6daf8086ee16e61e4
jerry1100 <jwu1100@gmail.com>
2018-12-16 15:13:44 -0800

    add 17

---
+++ 017-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,42 @@
+/**
+ * Time: O(n * 4^n)
+ * Space: O(n * 4^n)
+ * n - number of digits
+ */
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+function letterCombinations(digits) {
+  if (!digits) {
+    return [];
+  }
+
+  const solutions = [];
+  const LETTER_MAP = {
+    2: 'abc',
+    3: 'def',
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'pqrs',
+    8: 'tuv',
+    9: 'wxyz',
+  };
+
+  const getCombinations = (set, position) => {
+    if (position === digits.length) {
+      return solutions.push(set.join(''));
+    }
+    const letters = LETTER_MAP[digits.charAt(position)];
+    for (let i = 0; i < letters.length; i++) {
+      set.push(letters.charAt(i));
+      getCombinations(set, position + 1);
+      set.pop();
+    }
+  };
+
+  getCombinations([], 0);
+  return solutions;
+};
