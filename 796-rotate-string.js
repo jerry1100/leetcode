79224d830ed86e8317165d4962b08141b5247434
jerry1100 <jwu1100@gmail.com>
2018-12-25 00:16:19 -1000

    add 796

---
+++ 796-rotate-string.js
@@ -0,0 +1,20 @@
+/**
+ * Time: O(n)
+ * Space: O(1)
+ * n - length of strings
+ */
+
+/**
+ * @param {string} A
+ * @param {string} B
+ * @return {boolean}
+ */
+function rotateString(A, B) {
+  if (A.length !== B.length) {
+    return false;
+  }
+
+  // If B is a rotation of A, then B can be found in AA
+  // E.g. A = foobar, B = oobarf, AA = f(oobarf)oobar
+  return A.repeat(2).includes(B);
+}
