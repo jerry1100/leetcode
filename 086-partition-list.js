91e29980f8a2eb993f39bbc74ac468b40d9a63dd
jerry1100 <jwu1100@gmail.com>
2018-12-26 17:34:59 -1000

    add 86

---
+++ 086-partition-list.js
@@ -0,0 +1,41 @@
+/**
+ * Time: O(n)
+ * Space: O(1)
+ * n - # of nodes
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+function partition(head, x) {
+  let leftHead = { next: null };
+  let rightHead = { next: null };
+  let left = leftHead;
+  let right = rightHead;
+
+  // Split list into two sides, left if val < x, else right
+  for (let node = head; node !== null; node = node.next) {
+    if (node.val < x) {
+      left.next = node;
+      left = node;
+    } else {
+      right.next = node;
+      right = node;
+    }
+  }
+
+  // Combine the two sides
+  left.next = rightHead.next;
+  right.next = null;
+
+  return leftHead.next;
+}
