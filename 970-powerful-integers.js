87ff5f15a52b0c2643c17812e25112ff19f3fc63
jerry1100 <jwu1100@gmail.com>
2019-01-05 23:54:33 -1000

    add 970

---
+++ 970-powerful-integers.js
@@ -0,0 +1,33 @@
+/**
+ * Time: O(log(x, bound) * log(y, bound)), where log(a, b) is log base a of b
+ * Space: O(log(x, bound) * log(y, bound))
+ * x - value of x
+ * y - value of y
+ * bound - value of bound
+ */
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} bound
+ * @return {number[]}
+ */
+function powerfulIntegers(x, y, bound) {
+  const set = new Set();
+
+  for (let valX = 1; valX < bound; valX *= x) {
+    for (let valY = 1; valX + valY <= bound; valY *= y) {
+      set.add(valX + valY);
+
+      if (y === 0 || y === 1) {
+        break;
+      }
+    }
+
+    if (x === 0 || x === 1) {
+      break;
+    }
+  }
+
+  return Array.from(set);
+}
