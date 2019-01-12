a3296888c3f09ad2ef631829df156651773b994c
jerry1100 <jwu1100@gmail.com>
2019-01-11 22:21:49 -0800

    refactor 14

--- 014-longest-common-prefix.js
+++ 014-longest-common-prefix.js
@@ -14,22 +14,26 @@ function longestCommonPrefix(strs) {
     return '';
   }
 
-  // Method to recursively get the lcp starting at index
-  const lcpAtIndex = index => {
-    const charToTest = strs[0].charAt(index);
-    if (!charToTest) {
+  return lcpAtIndex(strs, 0);
+}
+
+/**
+ * @param {string[]} strs
+ * @param {number} index
+ * @return {string}
+ */
+function lcpAtIndex(strs, index) {
+  const charToTest = strs[0].charAt(index);
+  if (!charToTest) {
+    return '';
+  }
+
+  // Check if all strings match the test char
+  for (let i = 1; i < strs.length; i++) {
+    if (strs[i].charAt(index) !== charToTest) {
       return '';
     }
+  }
 
-    // Check if all strings match the test char
-    for (let i = 1; i < strs.length; i++) {
-      if (strs[i].charAt(index) !== charToTest) {
-        return '';
-      }
-    }
-
-    return charToTest + lcpAtIndex(index + 1);
-  };
-
-  return lcpAtIndex(0);
+  return charToTest + lcpAtIndex(strs, index + 1);
 }
