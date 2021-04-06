69cb0224923c013df28814bfb8bb7283079bac4f
jerry1100 <jwu1100@icloud.com>
2021-04-05 20:25:06 -1000

    141

--- 141-linked-list-cycle.js
+++ 141-linked-list-cycle.js
@@ -1,30 +1,60 @@
-/**
- * Time: O(n)
- * Space: O(1)
- * n - # of nodes
- */
+/*
+  Solution:
+    Store nodes in a set and check if we've seen the node before
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-
-/**
- * @param {ListNode} head
- * @return {boolean}
+  Analysis:
+    Time: O(n)
+    Space: O(n)
  */
 function hasCycle(head) {
-  let slow = head;
-  let fast = head;
+  const seen = new Set();
 
-  while (fast && fast.next) {
+  while (head) {
+    if (seen.has(head)) {
+      return true;
+    }
+
+    seen.add(head);
+
+    head = head.next;
+  }
+
+  return false;
+}
+
+/*
+  Solution 2:
+    Another solution that doesn't use any extra memory is the fast & slow pointers approach. Have
+    one pointer, fast, that moves two nodes each time, whereas the slow pointer moves one node each
+    time. If fast and slow ever meet, then there is a cycle.
+
+  Algorithm:
+    fast = head
+    slow = head
+
+    while (fast.next)
+      fast = fast.next.next
+      slow = slow.next
+
+      if (fast === slow) {
+        return true;
+      }
+
+    return false;
+
+  Analysis:
+    Time: O(n)
+    Space: O(1)
+ */
+function hasCycle(head) {
+  let fast = head;
+  let slow = head;
+
+  while (fast?.next) {
     fast = fast.next.next;
     slow = slow.next;
 
-    if (slow === fast) {
+    if (fast === slow) {
       return true;
     }
   }
