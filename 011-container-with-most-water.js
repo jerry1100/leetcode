ff6b5398d448ed4205c57a60f4202a626d9360c6
jerry1100 <jwu1100@icloud.com>
2021-04-05 22:05:21 -1000

    11

--- 011-container-with-most-water.js
+++ 011-container-with-most-water.js
@@ -1,31 +1,95 @@
-/**
- * Time: O(n)
- * Space: O(1)
- * n - number of points
- */
+/*
+  Naive solution:
+    Try out all combinations of start and stop positions, calculating the area based on index and
+    the minimum heights between the two.
 
-/**
- * @param {number[]} height
- * @return {number}
+  Algorithm:
+    maxSoFar = 0
+
+    for start = 0; start < height.length - 1; start++
+      for end = start + 1; end < height.length; end++
+        areaHeight = min(height[start], height[end])
+        areaWidth = end - start
+
+        maxSoFar = max(maxSoFar, areaHeight * areaWidth)
+
+    return maxSoFar
+
+  Analysis:
+    Time: O(n^2)
+    Space: O(1)
+
+  This solution exceeded the time limit on leetcode, so try the optimized one.
  */
 function maxArea(height) {
-  let left = 0;
-  let right = height.length - 1;
-  let maxArea = 0;
+  let maxSoFar = 0;
 
-  while (left < right) {
-    const area = Math.min(height[left], height[right]) * (right - left);
-    if (area > maxArea) {
-      maxArea = area;
-    }
+  for (let start = 0; start < height.length - 1; start++) {
+    for (let end = start + 1; end < height.length; end++) {
+      const areaHeight = Math.min(height[start], height[end]);
+      const areaWidth = end - start;
 
-    // Move the shorter line, this is the only way to increase the area
-    if (height[left] < height[right]) {
-      left += 1;
-    } else {
-      right -= 1;
+      maxSoFar = Math.max(maxSoFar, areaHeight * areaWidth);
     }
   }
 
-  return maxArea;
+  return maxSoFar;
+}
+
+/*
+  Optimal solution:
+    The crux of the problem is: how do we know what to include in our window?
+
+    After some thinking, I came up with a potential solution using two pointers, one at each end.
+    The idea is that we'll move the pointer with the smallest height inward, while calculating the
+    area using the smaller of the two heights, the distance between them, and keeping track of the
+    max area so far.
+
+    I believe this type of problem is a greedy algorithm, where we make decisions to maximize some
+    value, eventually leading to the global maximum.
+
+    The decision being made here is figuring out which pointer we should move forward. Obviously, it
+    would be unwise to move the pointer with the bigger height, so we move the pointer with the
+    smaller height to try and increase our area. Eventually, we should come across the maximum area
+    possible.
+
+  Algorithm:
+    start = 0
+    end = n-1
+    maxSoFar = 0
+
+    while start < end
+      areaHeight = min(height[start], height[end])
+      areaWidth = end - start
+      maxSoFar = max(maxSoFar, areaHeight * areaWidth)
+
+      if height[start] < height[end]
+        start++;
+      else
+        end--;
+
+  Analysis:
+    Time: O(n)
+    Space: O(1)
+ */
+function maxArea(height) {
+  let start = 0;
+  let end = height.length - 1;
+  let maxSoFar = 0;
+
+  while (start < end) {
+    const areaHeight = Math.min(height[start], height[end]);
+    const areaWidth = end - start;
+    const area = areaHeight * areaWidth;
+
+    maxSoFar = Math.max(maxSoFar, area);
+
+    if (height[start] < height[end]) {
+      start++;
+    } else {
+      end--;
+    }
+  }
+
+  return maxSoFar;
 }
