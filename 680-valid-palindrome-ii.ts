075a73bb5e7b777b566d3ead9da10a50f663d3e0
jerry1100 <jwu1100@icloud.com>
2021-06-16 02:31:51 -1000

    680

---
+++ 680-valid-palindrome-ii.ts
@@ -0,0 +1,32 @@
+function validPalindrome(s: string): boolean {
+    let left = 0;
+    let right = s.length - 1;
+    
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return isPalindrome(s, left + 1, right)
+                || isPalindrome(s, left, right - 1);
+        }
+        
+        left++;
+        right--;
+    }
+    
+    return true;
+};
+
+function isPalindrome(s: string, start: number, stop: number): boolean {
+    let left = start;
+    let right = stop;
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false;
+        }
+        
+        left++;
+        right--;
+    }
+    
+    return true;
+}
