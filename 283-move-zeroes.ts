96ad4e58f31eb03063bedf1890cef38c3f4685bb
jerry1100 <jwu1100@icloud.com>
2021-06-21 22:03:27 -1000

    283

---
+++ 283-move-zeroes.ts
@@ -0,0 +1,26 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums: number[]): void {
+    const iFirstZero = nums.indexOf(0);
+
+    // No 0s to move
+    if (iFirstZero === -1) {
+        return;
+    }
+
+    // Swap with last 0 we saw
+    let iLastZero = iFirstZero;
+    for (let i = iFirstZero + 1; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            swap(nums, i, iLastZero);
+            iLastZero++;
+        }
+    }
+};
+
+function swap(nums: number[], i1: number, i2: number): void {
+    const tmp = nums[i1];
+    nums[i1] = nums[i2];
+    nums[i2] = tmp;
+}
