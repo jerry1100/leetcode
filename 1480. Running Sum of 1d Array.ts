7d275c088859a4427d3a88169b8379f246bbbfd2
jerry1100 <jwu1100@icloud.com>
2021-06-30 16:06:01 -1000

    1480

---
+++ 1480. Running Sum of 1d Array.ts
@@ -0,0 +1,13 @@
+// N: length of nums
+// Time: (N)
+// Space: O(1) (space used for answer is ignored)
+
+function runningSum(nums: number[]): number[] {
+    const sums = [nums[0]];
+
+    for (let i = 1; i < nums.length; i++) {
+        sums[i] = sums[i - 1] + nums[i];
+    }
+    
+    return sums;
+};
