0f13ed9c2ec51f8a7ed3888a2d4e1e7041fbede5
jerry1100 <jwu1100@icloud.com>
2021-07-03 22:23:43 -1000

    169

---
+++ 169. Majority Element.ts
@@ -0,0 +1,26 @@
+// N: number of nums
+// Time: O(N)
+// Space: O(1)
+
+// Using Boyer-Moore algorithm:
+//  1) Set candidate to initial element
+//  2) If next number equals current candidate, add 1 to count. Else subtract 1.
+//  3) When count equals 0, assign a new candidate.
+// The Boyer-Moore algorithm says that after processing all the elements, the
+// resulting candidate will be the majority element. This relies on there being
+// more +1s than -1s since the majority element is the majority. It can also be
+// used to determine voting results.
+function majorityElement(nums: number[]): number {
+    let candidate: number | undefined;
+    let count = 0;
+
+    for (const num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+
+        count += candidate === num ? 1 : -1;
+    }
+
+    return candidate;
+}
