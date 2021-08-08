5214f95997de8665479e9d70bfd43a6f9a7a351d
jerry1100 <jwu1100@icloud.com>
2021-08-08 11:27:22 -1000

    377

---
+++ 377. Combination Sum IV.ts
@@ -0,0 +1,24 @@
+// N: target
+// M: number of nums
+// Time: O(N*M)
+// Space: O(N)
+
+function combinationSum4(nums: number[], target: number): number {
+    const memo = new Array(target + 1).fill(0);
+
+    // One combination that adds up to 0: empty set
+    memo[0] = 1;
+
+    // numCombos[i] = numCombos[i - num] for all num <= i
+    // E.g. the number of ways we can make 10 with [2, 5] is the sum of the
+    // number of ways we can make (10 - 2 = 8) and (10 - 5 = 5) with [2, 5]
+    for (let i = 1; i <= target; i++) {
+        for (const num of nums) {
+            if (num <= i) {
+                memo[i] += memo[i - num];
+            }
+        }
+    }
+
+    return memo[target];
+}
