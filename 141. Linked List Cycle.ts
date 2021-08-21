88e56604c499e5ad9d2f585603231f9985b62d64
jerry1100 <jwu1100@icloud.com>
2021-08-20 15:07:42 -1000

    141

---
+++ 141. Linked List Cycle.ts
@@ -0,0 +1,31 @@
+// N: number of nodes
+// Time: O(N)
+// Space: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+    let slow = head;
+    let fast = head;
+
+    while (fast?.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow === fast) {
+            return true;
+        }
+    }
+
+    return false;
+}
